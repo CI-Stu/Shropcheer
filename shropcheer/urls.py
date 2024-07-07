6993d4e503fe10044fce84eae57ae7099f65281f
Stuart Preston <stupreston89@gmail.com>
2024-07-07 15:30:53 +0000

    add allauth and basic html code for sign up and registration

--- shropcheer/urls.py
+++ shropcheer/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path("accounts/", include("allauth.urls")),
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
     path("", include("stories.urls"), name='stories-urls'),
