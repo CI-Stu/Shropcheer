5a2eb358f159b0de477f98d59bb67f21569b89b4
Stuart Preston <stupreston89@gmail.com>
2024-07-07 11:06:08 +0000

    added story_detail view

--- stories/urls.py
+++ stories/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.StoryList.as_view(), name='home'),
+    path('<slug:slug>/', views.story_detail, name='story_detail'),
 ]
--- stories/views.py
+++ stories/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Story
 
@@ -9,3 +9,27 @@ class StoryList(generic.ListView):
     queryset = Story.objects.all()
     template_name = "stories/index.html"
     paginate_by = 6
+
+
+def story_detail(request, slug):
+    """
+    Display an individual :model:`stories.Story`.
+
+    **Context**
+
+    ``post``
+        An instance of :model:`stories.Story`.
+
+    **Template:**
+
+    :template:`stories/story_detail.html`
+    """
+
+    queryset = Story.objects.filter(status=1)
+    story = get_object_or_404(queryset, slug=slug)
+
+    return render(
+        request,
+        "stories/story_detail.html",
+        {"story": story},
+    )
