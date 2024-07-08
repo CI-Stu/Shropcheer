9361b8222bfac39b23bf163c09e7107e3f0af7e0
Stuart Preston <stupreston89@gmail.com>
2024-07-08 12:06:16 +0000

    post comments when user is logged in

--- stories/views.py
+++ stories/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
+from django.contrib import messages
 from .models import Story
+from .forms import CommentForm
 
 # Create your views here.
 
@@ -30,6 +32,18 @@ def story_detail(request, slug):
     comments = story.comments.all().order_by("created_on")
     comment_count = story.comments.count()
 
+    if request.method == "POST":
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.name = request.user
+            comment.story = story
+            comment.save()
+            
+
+
+    comment_form = CommentForm()
+
     return render(
         request,
         "stories/story_detail.html",
@@ -37,5 +51,6 @@ def story_detail(request, slug):
             "story": story,
             "comments": comments,
             "comment_count": comment_count,
+            "comment_form": comment_form,
         },
     )
