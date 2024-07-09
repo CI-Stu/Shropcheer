a686dba194212401ab4dfbaa57b11781ef976108
Stuart Preston <stupreston89@gmail.com>
2024-07-09 15:51:41 +0000

    update name of author in story model

--- stories/models.py
+++ stories/models.py
@@ -49,7 +49,7 @@ class Story(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(
-    User, on_delete=models.CASCADE, related_name="stories_storys"
+    User, on_delete=models.CASCADE, related_name="story_author"
     )
     body = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
